78c398bc5b92a3133d335089bc2cb3d0588eb7f6
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-01-13 16:29:30 +0500

    adding css

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
+import './normalize.css';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className='sidemenu'>
+        <div className='side-menu-button'>
+          <span>+</span>
+          New chat
+        </div>
+      </aside>
+      <section className='chatbox'></section>
     </div>
   );
 }
