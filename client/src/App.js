b0b4d751b24f05bf6623e1de9e350fa7afaabadc
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-01-13 18:12:44 +0500

    adding style for chat box

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './normalize.css';
+import logo from './img/logo.png';
 
 function App() {
   return (
@@ -10,7 +11,29 @@ function App() {
           New chat
         </div>
       </aside>
-      <section className='chatbox'></section>
+      <section className='chatbox'>
+        <div className='chat-log'>
+          <div className='chat-message'>
+            <div className='chat-message-center'>
+              <div className='avatar'></div>
+              <div className='message'>message</div>
+            </div>
+          </div>
+
+          <div className='chat-message chatgpt'>
+            <div className='chat-message-center'>
+              <div className='avatar chatgpt'></div>
+              <div className='message'>ai</div>
+            </div>
+          </div>
+        </div>
+        <div className='chat-input-holder'>
+          <textarea row='1' className='chat-input-textarea'></textarea>
+        </div>
+        <footer className='footer'>
+          <p>ChatGPT - Mikaeels</p>
+        </footer>
+      </section>
     </div>
   );
 }
